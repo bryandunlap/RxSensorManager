d972f09d8eb1aada82fa50ebdd566730fda19303
Bryan Dunlap <bryandunlap@gmail.com>
2016-12-30 17:18:45 -0700

    Initial skeleton example app module. Reorganized code quality files. README.md update.

---
+++ examples/src/main/java/net/bryandunlap/rxsensormanager/examples/MainActivity.java
@@ -0,0 +1,13 @@
+package net.bryandunlap.rxsensormanager.examples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
